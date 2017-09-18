7cfc6ae764b8a6de75f0b8620d833de7400db446
MARTINA_WEIDENBAUM <martinaweidenbaum1@MARTINA-WEIDENBAUMs-MacBook-Pro.local>
2017-09-18 15:34:38 -0400

    Done.

--- src/components/ImageSlider.js
+++ src/components/ImageSlider.js
@@ -1,1 +1,23 @@
-// Bomb Component Code Goes Here
+// image_slider Component Code Goes Here
+
+import React from 'react';
+ 
+class ImageSlider extends React.Component { 
+  constructor() {
+    super();
+ 
+    this.state = {
+      currentSlideIndex: 0
+    }
+  }
+ 
+  render() {
+    return (
+      <div className="image-slider">
+        I am on slide {this.state.currentSlideIndex}
+      </div>
+    )
+  }
+}
+ 
+export default ImageSlider;
